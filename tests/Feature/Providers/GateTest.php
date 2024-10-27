225d93724fb278c52765aae83f33cb27dd70c376
Arief-Karditya <nolifegansaya@gmail.com>
2024-10-27 13:26:51 +0700

    Gate Facade

--- tests/Feature/Providers/GateTest.php
+++ tests/Feature/Providers/GateTest.php
@@ -26,21 +26,21 @@ class GateTest extends TestCase
     {
         $this->seed([UserSeeder::class, ContactSeeder::class]);
 
-        $user = User::where('email','terry@localhost')->first();
+        $user = User::where('email', 'terry@localhost')->first();
         self::assertNotNull($user, );
         Log::info(json_encode($user, JSON_PRETTY_PRINT));
 
         Auth::login($user);
 
-        $contact = Contact::where('email','test@localhost')->first();
+        $contact = Contact::where('email', 'test@localhost')->first();
         self::assertNotNull($contact, );
         Log::info(json_encode($contact, JSON_PRETTY_PRINT));
-        
+
         self::assertTrue(Gate::allows("get-contact", $contact));
         self::assertTrue(Gate::allows("update-contact", $contact));
         self::assertTrue(Gate::allows("delete-contact", $contact));
 
-        $user2 = User::where('email','andrew@localhost')->first();
+        $user2 = User::where('email', 'andrew@localhost')->first();
         self::assertNotNull($user2, );
         Log::info(json_encode($user2, JSON_PRETTY_PRINT));
 
@@ -49,5 +49,41 @@ class GateTest extends TestCase
         self::assertFalse(Gate::allows("get-contact", $contact));
         self::assertFalse(Gate::allows("update-contact", $contact));
         self::assertFalse(Gate::allows("delete-contact", $contact));
+    }
+
+    public function testGateFacadeMethod(): void
+    {
+        self::seed([UserSeeder::class, ContactSeeder::class]);
+        # code
+        $user = User::where('email', '=', 'terry@localhost')->first();
+        self::assertNotNull($user, );
+        Log::info(json_encode($user, JSON_PRETTY_PRINT));
+        # code
+        Auth::login($user);
+
+        $contact = Contact::where('email', '=', 'test@localhost')->first();
+        self::assertNotNull($contact, );
+        Log::info(json_encode($contact, JSON_PRETTY_PRINT));
+
+        # code
+        self::assertTrue(Gate::any(["get-contact", "delete-contact", "update-contact"], $contact));
+        self::assertFalse(Gate::none(["get-contact", "delete-contact", "update-contact"], $contact));
+    }
+
+    public function testGateAllowsUserNotYetLogin()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+
+        $user = User::where('email', '=', 'terry@localhost')->first();
+        Log::info(json_encode($user, JSON_PRETTY_PRINT));
+
+        $gate = Gate::forUser($user);
+
+        $contact = Contact::where('user_id', '=', $user->id)->first();
+        Log::info(json_encode($contact, JSON_PRETTY_PRINT));
+
+        self::assertTrue($gate->allows('get-contact', $contact));
+        self::assertTrue($gate->allows("update-contact", $contact));
+        self::assertTrue($gate->allows("delete-contact", $contact));
     }
 }
